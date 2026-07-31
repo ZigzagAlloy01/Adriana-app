fe3e11a9b079986955ba563944eac2e645a9db92
ZigzagAlloy01 <ivanblued@gmail.com>
2026-07-30 21:16:52 -0600

    Send code Five minutes timer 1

--- src/pages/onboarding/CreateCouple.tsx
+++ src/pages/onboarding/CreateCouple.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { createCouple } from "@/services/couples";
 import { getErrorMessage } from "@/services/helpers";
